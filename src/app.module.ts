3d1c6877db537b40ad514ce0c4129a1540d43dcd
arielcorte <cortearielformal@gmail.com>
2023-08-01 19:00:24 -0300

    remove unused controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WhoamiController } from './whoami.controller';
 import { WhoamiService } from './whoami.service';
-import { WhatsappController } from './whatsapp.controller';
 import { WhatsappService } from './whatsapp.service';
 import { WhatsappGateway } from './websocket/whatsapp.gateway';
 import { ChatflowService } from './chatflow.service';
@@ -11,7 +10,7 @@ import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [HttpModule],
-  controllers: [AppController, WhoamiController, WhatsappController],
+  controllers: [AppController, WhoamiController],
   providers: [
     AppService,
     WhoamiService,
