8fe869a710531bad4744868b0e6068e150b62c77
Ariel Corte <cortearielformal@gmail.com>
2023-08-31 21:26:33 -0300

    change name to flowiseapi

---
+++ src/chatbot/flowiseapi.service.ts
@@ -0,0 +1,49 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable()
+export class FlowiseApiService {
+  constructor(private readonly httpService: HttpService) {}
+
+  async query(data: {
+    question: string;
+    sessionId: string;
+    clientApi: { url: string; key: string };
+    author?: string;
+    to?: string;
+  }): Promise<string> {
+    console.log('quering', data.clientApi);
+    console.log(data);
+    const clientApiUrl = data.clientApi.url.startsWith('http')
+      ? 'http://flowise:3000' + data.clientApi.url.replace(/.*\/api/g, '/api')
+      : 'http://flowise:3000' + data.clientApi.url;
+    try {
+      const response = await lastValueFrom(
+        this.httpService.post(
+          clientApiUrl,
+          {
+            question:
+              data.author && data.to
+                ? `${data.author} dijo a ${data.to}: ${data.question}`
+                : data.question,
+            overrideConfig: { sessionId: data.sessionId },
+          },
+          {
+            headers: {
+              Authorization: data.clientApi.key,
+              'Content-Type': 'application/json',
+            },
+          },
+        ),
+      );
+      //const response = await lastValueFrom(
+      //  this.httpService.get('https://pokeapi.co/api/v2/pokemon/ditto'),
+      //);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      return 'failed';
+    }
+  }
+}
